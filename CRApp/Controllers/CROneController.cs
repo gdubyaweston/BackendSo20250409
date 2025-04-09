f7e7897379f33e453e470ec6e45c65d79c7b852e
gweston@aictitle.com <gweston@aictitle.com>
2025-04-09 12:46:58 -0500

    Testing add classes

---
+++ CRApp/Controllers/CROneController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CROneController : ControllerBase
+    {
+    }
+}
