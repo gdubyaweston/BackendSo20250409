e906a05fc8d3c62e0199d48105c8b34b63b76a14
gweston@aictitle.com <gweston@aictitle.com>
2025-04-11 12:47:03 -0500

    added more login

--- LoginApp/Controllers/LoginController.cs
+++ LoginApp/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using LoginApp.Code;
+using ServiceData;
+using LoginData;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -37,9 +39,67 @@ namespace LoginApp.Controllers
         #endregion
 
         #region Public Methods
+
+        [HttpPost("userlogin")]
+        public async Task<ServiceResponse<LoginResult>> Login([FromBody] Login li) 
+        {
+            Console.WriteLine("Login - Login");
+
+            return await LoginTheUser(li);
+                        
+        }
+
+        [HttpPost("twostep")]
+        public async Task<ServiceResponse<LoginResult>> LoginTwoStep([FromBody] Login li)
+        {
+            Console.WriteLine("Login - Login");
+
+            return await LoginTheTwoStep(li);
+
+        }
+
         #endregion
 
         #region Private Methods
+
+        private async Task<ServiceResponse<LoginResult>> LoginTheUser(Login li) 
+        {
+            Console.WriteLine("LoginTheUser - LoginTheUser");
+
+            try
+            {
+                var vUser = await ValidUser(li);
+                if (vUser.Equals(true))
+                {
+                    return new ServiceResponse<LoginResult>() { Data = null, Message = "Login Invalid vUser.", Success = false };
+                }
+                return new ServiceResponse<LoginResult>() { Data = null, Message = "Login Invalid sign in information provided.", Success = false };
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResponse<LoginResult>() { Data = null, Message = "Login Error.<br />" + ex.Message, Success = false };
+            }
+        }
+
+        private async Task<bool> ValidUser(Login li) 
+        {
+            return false;
+        }
+
+        private async Task<ServiceResponse<LoginResult>> LoginTheTwoStep(Login li)
+        {
+            Console.WriteLine("LoginTheTwoStep - LoginTheTwoStep");
+
+            try
+            {
+                return new ServiceResponse<LoginResult>() { Data = null, Message = "Login Two Step Invalid sign in information provided.", Success = false };
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResponse<LoginResult>() { Data = null, Message = "Login Two Step Error.<br />" + ex.Message, Success = false };
+            }
+        }
+
         #endregion
 
     }
